efba52ff893e99bc3cb62888c32de10cf06c0fff
Anthony Webbs <ukwelister@gmail.com>
2021-10-09 20:09:01 +0300

    Add TestAssertMapEquals()
    Add test function TestAssertMapEquals that check if two map contents match or are equal.

--- week02/assignment02_test.go
+++ week02/assignment02_test.go
@@ -40,3 +40,38 @@ func TestAssertSliceEquals(t *testing.T) {
 		}
 	}
 }
+
+func TestAssertMapEquals(t *testing.T) {
+	exp := map[string]string{
+		"John":   "Football",
+		"Paul":   "Basketball",
+		"Mary":   "Ice Hockey",
+		"George": "Cricket",
+	}
+
+	act := map[string]string{}
+
+	//Copy exp values into the act variable
+	for key, values := range exp {
+		act[key] = values
+	}
+
+	//Check if length of act and exp are the same
+	if len(exp) != len(act) {
+		t.Fatalf("unexpected value, got: %d, exp: %d", len(act), len(exp))
+	}
+
+	//Check if act map content equals to exp
+	for i, v := range act {
+
+		_, ok := exp[i]
+
+		if !ok {
+			t.Fatalf("Key not found: %q", i)
+		}
+
+		if v != exp[i] {
+			t.Fatalf("unexpected value, got: %s, exp: %s at key %v", v, exp[i], i)
+		}
+	}
+}
