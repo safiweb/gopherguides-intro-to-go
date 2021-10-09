a22cdadfe88e34da459470feca11915c28690ac8
Anthony Webbs <ukwelister@gmail.com>
2021-10-09 20:00:44 +0300

    Add TestAssertSliceEquals()
    Add test function TestAssertSliceEquals that check if two slices contents match or are equal.

--- week02/assignment02_test.go
+++ week02/assignment02_test.go
@@ -18,3 +18,25 @@ func TestAssertArrayEquals(t *testing.T) {
 		}
 	}
 }
+
+func TestAssertSliceEquals(t *testing.T) {
+	exp := []string{"John", "Paul", "George", "Ringo"}
+	act := []string{}
+
+	//Copy exp values into the act variable
+	for _, v := range exp {
+		act = append(act, v)
+	}
+
+	//Check if length of act and exp are the same
+	if len(exp) != len(act) {
+		t.Fatalf("unexpected value, got: %d, exp: %d", len(act), len(exp))
+	}
+
+	//Check if act slice content equals to exp
+	for i, v := range act {
+		if v != exp[i] {
+			t.Fatalf("unexpected value, got: %s, exp: %s at index %v", v, exp[i], i)
+		}
+	}
+}
