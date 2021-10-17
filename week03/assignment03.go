1cf0fa66b57663abf0e53249ecbc96398e9fbfbc
Anthony Webbs <ukwelister@gmail.com>
2021-10-18 02:33:55 +0300

    Setting up Movie struct and it methods

---
+++ week03/assignment03.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type Movie struct {
+	Name    string
+	Length  int
+	rating  float32
+	plays   int
+	viewers int
+}
+
+func (m *Movie) Rate(rating float32) error {
+
+	if m.plays == 0 {
+		return fmt.Errorf("can't review a movie without watching it first")
+	}
+
+	m.rating = m.rating + rating
+	return nil
+
+}
+
+func (m *Movie) Play(viewers int) {
+	m.viewers = m.viewers + viewers
+	m.plays = m.plays + viewers
+}
+
+func (m Movie) Viewers() int {
+	return m.viewers
+}
+
+func (m Movie) Plays() int {
+	return m.plays
+}
+
+func (m Movie) Rating() float64 {
+	ratings := m.rating / float32(m.plays)
+	return float64(ratings)
+}
+
+func (m Movie) String() string {
+	s := fmt.Sprintf("%v (%dm) %.1f%%", m.Name, m.Length, m.rating)
+	return s
+}
+
+func main() {
+
+}
